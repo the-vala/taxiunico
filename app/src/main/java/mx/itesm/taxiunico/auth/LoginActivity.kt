bb80a22ea4b1168fb31f5fbb23cace0080648112
CarlosIvanCardenas <cicc1998@gmail.com>
2019-03-28 11:59:37 -0600

    Header

---
+++ app/src/main/java/mx/itesm/taxiunico/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package mx.itesm.taxiunico.auth
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import mx.itesm.taxiunico.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
