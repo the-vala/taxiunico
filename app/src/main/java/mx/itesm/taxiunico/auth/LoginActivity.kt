ff468a4527e6c861ec370b455fe3c3a07f801614
CarlosIvanCardenas <cicc1998@gmail.com>
2019-03-28 14:38:16 -0600

    firebase integration

--- app/src/main/java/mx/itesm/taxiunico/auth/LoginActivity.kt
+++ app/src/main/java/mx/itesm/taxiunico/auth/LoginActivity.kt
@@ -2,12 +2,26 @@ package mx.itesm.taxiunico.auth
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
 import mx.itesm.taxiunico.R
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 }
