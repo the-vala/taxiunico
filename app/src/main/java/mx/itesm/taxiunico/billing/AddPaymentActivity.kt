05b5adbdf691baa21e45067dee5aa6c80309191f
salvador <salvador.barboza.portillo@gmail.com>
2019-03-29 16:37:41 -0600

    Bind payments and user profile to db

--- app/src/main/java/mx/itesm/taxiunico/billing/AddPaymentActivity.kt
+++ app/src/main/java/mx/itesm/taxiunico/billing/AddPaymentActivity.kt
@@ -2,12 +2,29 @@ package mx.itesm.taxiunico.billing
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_add_payment.*
 import mx.itesm.taxiunico.R
 
 class AddPaymentActivity : AppCompatActivity() {
 
+    val paymentService = PaymentService()
+    val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_payment)
+
+        button.setOnClickListener {
+            savePaymentMethod()
+        }
+    }
+
+    private fun savePaymentMethod() {
+        val cardNumText = cardNum.text.toString()
+        paymentService.addMethod(auth.uid!!, PaymentMethod.Card(
+            last4Digits = cardNumText.substring(cardNumText.length - 4, cardNumText.length).toInt(),
+            expDate = expDateText.text.toString()
+        )) { finish() }
     }
 }
--- app/src/main/java/mx/itesm/taxiunico/billing/PaymentFormsActivity.kt
+++ app/src/main/java/mx/itesm/taxiunico/billing/PaymentFormsActivity.kt
@@ -4,32 +4,45 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_payment_forms.*
 import mx.itesm.taxiunico.R
 
 class PaymentFormsActivity : AppCompatActivity() {
 
+    private val paymentService = PaymentService()
+    private val auth = FirebaseAuth.getInstance()
+    private  val adapter = PaymentMethodAdapter(mutableListOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_forms)
 
-        val payments = mutableListOf(
-            PaymentMethod.Card(1234, "2020"),
-            PaymentMethod.Card(8336, "2020"),
-            PaymentMethod.Card(7861, "Asdfasdf"),
-            PaymentMethod.Cash
-        )
-
-        val adapter = PaymentMethodAdapter(mutableListOf())
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         recyclerView.adapter = adapter
-        adapter.setData(payments)
-
         button.setOnClickListener {
             startActivity(getAddPaymentIntent())
         }
     }
+
+
+
+    override fun onResume() {
+        super.onResume()
+
+        paymentService.getMethods(
+            auth.uid!!,
+            onComplete = ::updatePaymentCards
+        )
+    }
+
+    private fun updatePaymentCards(newList: MutableList<PaymentMethod.Card>) {
+        val list = mutableListOf<PaymentMethod>()
+        list.addAll(newList)
+        adapter.setData(list)
+    }
+
 
 
     fun getAddPaymentIntent(): Intent = Intent(this, AddPaymentActivity::class.java)
