a0b4b421592b9b1f0884c57d5f89904f203cadcd
Diego Valadez <the-vala@users.noreply.github.com>
2019-04-01 12:51:43 -0600

    Added funcitonality to show list of Encuestas

---
+++ app/src/main/java/mx/itesm/taxiunico/EncuestaAdapter.kt
@@ -0,0 +1,2 @@
+package mx.itesm.taxiunico
+
---
+++ app/src/main/java/mx/itesm/taxiunico/EncuestasActivity.kt
@@ -0,0 +1,12 @@
+package mx.itesm.taxiunico
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EncuestasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_encuestas)
+    }
+}
