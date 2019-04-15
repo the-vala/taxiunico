2d6a82dd1494015d4ea04680feb6fea3432d6640
salvador <salvador.barboza.portillo@gmail.com>
2019-04-15 14:17:45 -0500

    revert to f7e376d65dfd099cf4fccc86c715427b5cd588ee

--- app/src/main/java/mx/itesm/taxiunico/MainActivity.kt
+++ app/src/main/java/mx/itesm/taxiunico/MainActivity.kt
@@ -1,12 +1,29 @@
 package mx.itesm.taxiunico
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import mx.itesm.taxiunico.auth.LoginActivity
+import mx.itesm.taxiunico.billing.AddPaymentActivity
+import mx.itesm.taxiunico.billing.PaymentFormsActivity
+import mx.itesm.taxiunico.billing.PaymentMethod
+import mx.itesm.taxiunico.profile.UserProfile
+import mx.itesm.taxiunico.profile.UserProfileActivity
+import mx.itesm.taxiunico.profile.UserService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        startActivity(Intent(this, LoginActivity::class.java))
+
+        openProfile.setOnClickListener {
+            startActivity(Intent(this, PaymentFormsActivity::class.java))
+        }
     }
 }
