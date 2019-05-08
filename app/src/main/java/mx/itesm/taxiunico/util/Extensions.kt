51732fa04c672960e8d6931257f2666242365a6e
ariel-ms <arielmendez735@gmail.com>
2019-05-08 12:03:05 -0500

    Commets of util directory

--- app/src/main/java/mx/itesm/taxiunico/util/Extensions.kt
+++ app/src/main/java/mx/itesm/taxiunico/util/Extensions.kt
@@ -5,17 +5,29 @@ import com.google.firebase.firestore.GeoPoint
 import java.text.SimpleDateFormat
 import java.util.*
 
-
+/**
+ * Regresa un GeoPoint de firebase con latitud y logitud
+ * */
 fun LatLng.toGeoPoint(): GeoPoint {
     return GeoPoint(this.latitude, this.longitude)
 }
 
+/**
+ * @return objetivo SimpleDateFormat con el formato
+ * yyyy-MM-dd definido
+* */
 fun String.toLocalDate(): Date {
     return SimpleDateFormat("yyyy-MM-dd").parse(this)
 }
 
+/**
+ * @return un LatLng con latitud y logitud
+ * */
 fun GeoPoint.toLatLng(): LatLng {
     return LatLng(this.latitude, this.longitude)
 }
 
+/**
+ * converite el string en lowercase y despues lo capitaliza.
+ * */
 fun String.toSentenceCase(): String = this.toLowerCase().capitalize()
--- app/src/main/java/mx/itesm/taxiunico/util/Validator.kt
+++ app/src/main/java/mx/itesm/taxiunico/util/Validator.kt
@@ -2,9 +2,12 @@ package mx.itesm.taxiunico.util
 
 abstract class Validator {
     companion object {
+        /**
+         * Confirma que el reservation code tenga una logitud superior a 6
+         * */
         fun  valReservationCode(code:String):Boolean {
             if (code.length  < 6) {
-                return false //
+                return false
             }
             return true
         }
