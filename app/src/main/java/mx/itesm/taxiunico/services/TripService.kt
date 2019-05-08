8d0c0d1d1451999946e0a080a42eec2392b3821a
CarlosIvanCardenas <cicc1998@gmail.com>
2019-05-07 20:15:47 -0500

    Fix survey issues

--- app/src/main/java/mx/itesm/taxiunico/services/TripService.kt
+++ app/src/main/java/mx/itesm/taxiunico/services/TripService.kt
@@ -65,3 +65,8 @@ class TripService {
         const val TRIP_COLLECTION_KEY = "trips"
     }
 }
+
+sealed class Result<out T: Any> {
+    data class Success<out T: Any>(val result: T): Result<T>()
+    data class Failure(val result: Throwable?): Result<Nothing>()
+}
