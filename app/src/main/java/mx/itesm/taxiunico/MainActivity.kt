8602f9435f634df1c8ac95d0ea57ff510be59bcd
ariel-ms <arielmendez735@gmail.com>
2019-05-08 22:31:30 -0500

    update Snackbar bug

--- app/src/main/java/mx/itesm/taxiunico/MainActivity.kt
+++ app/src/main/java/mx/itesm/taxiunico/MainActivity.kt
@@ -137,9 +137,9 @@ class MainActivity : AppCompatActivity(),
     private fun showConnectionMessage(isConnected: Boolean) {
         if (!isConnected) {
             val message = "No hay conexion."
-            mSnackBar = Snackbar.make(findViewById(R.id.snackbar_container), message, Snackbar.LENGTH_LONG)
-            mSnackBar?.duration = Snackbar.LENGTH_INDEFINITE
-            val view = mSnackBar!!.view
+            snackBar = Snackbar.make(findViewById(R.id.snackbar_container), message, Snackbar.LENGTH_LONG)
+            snackBar?.duration = Snackbar.LENGTH_INDEFINITE
+            val view = snackBar!!.view
             val params = view.layoutParams as FrameLayout.LayoutParams
             params.gravity = Gravity.TOP
             view.layoutParams = params
